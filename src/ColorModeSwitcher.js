137112b360f0b84d4c3392c2a2cfa270cfee8df4
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-08 00:01:50 +0530

    few properties update in ColorModeSwitcher

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-export const ColorModeSwitcher = props => {
+const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
+      // size="md"
+      // fontSize="lg"
+      // aria-label={`Switch to ${text} mode`}
+      // marginLeft="2"
       variant="ghost"
       color="current"
-      marginLeft="2"
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
+      pos={'fixed'}
+      top={4} // here 1 unit = 4px
+      right={4}
       {...props}
     />
   );
 };
+
+export default ColorModeSwitcher;
